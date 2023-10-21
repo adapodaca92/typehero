4e3c3bdf5b322ca97ca5ef8c87f972f34e5a3c70
Anthony Apodaca <adapodaca92@gmail.com>
2023-10-20 17:42:47 -0700

    Changed anchor tag to render as span to avoid nesting anchor tags.

--- packages/ui/src/components/markdown.tsx
+++ packages/ui/src/components/markdown.tsx
@@ -48,7 +48,7 @@ export function Markdown({ children, className }: { children: string; className?
       className={className}
       components={{
         a: ({ className, ...props }) => (
-          <a className={clsx(className, 'text-blue-500')} {...props} />
+          <span className={clsx(className, 'text-blue-500')} {...props} />
         ),
         ul: ({ className, ...props }) => (
           <ul className={clsx(className, 'list-disc ps-10')} {...props} />
